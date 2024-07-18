3640bcd57291d63d867c7132dbe789d1f20e083f
Angelo <gangelos630@gmail.com>
2024-07-18 05:37:37 -0500

    routes(views)

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-// http://localhost:2609/tracks
+// GET http://localhost:2609/tracks
+router.get("/", (req, res) => {
+  const dataXample = ["track1", "track2", "track3"];
+  res.send({ dataXample });
+});
 
 module.exports = router;
